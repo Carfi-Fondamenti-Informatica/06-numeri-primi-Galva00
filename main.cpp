e90724b0a65dc85601e7ba222a88640464d9ce69
Galva00 <116543265+Galva00@users.noreply.github.com>
2022-12-01 23:06:27 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+  int n;
+  if (primi(n)==2) {
+    cout <<"numero primo"<<endl;
+  } else { 
+    cout <<"numero non primo"<<endl;
+  }
   return 0;
 }
